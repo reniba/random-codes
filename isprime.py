5db6963ce61f59f1af36aa729a8004429cf31a56
reniba <112177930+reniba@users.noreply.github.com>
2022-12-28 12:39:44 -0300

    Update isprime.py

--- isprime.py
+++ isprime.py
@@ -1,10 +1,8 @@
 import math
 def isprime(num):
+    """return True if the number called is prime"""
     for a in range(2,math.floor(num**0.5)+1):
         if num % a == 0:
             return False
     return True
 
-for n in range(3,500,2):
-    if isprime(n):
-        print(n)
