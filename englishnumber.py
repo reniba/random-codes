962e01ab9249877552815992a6eddf3384de282e
reniba <112177930+reniba@users.noreply.github.com>
2022-12-28 12:32:34 -0300

    Update englishnumber.py

--- englishnumber.py
+++ englishnumber.py
@@ -1,4 +1,30 @@
-#Works till the 5 digit
+#Works till the 9th digit
+
+
+def englishnumber(number):
+    """facilitates to call number names to make anothers number names"""
+    if number == 0:
+        return 'zero'
+    else:
+        number = remove_0(str(number))
+        if len(str(number)) <= 2:
+            return two_numbers(number)
+
+        if len(str(number)) == 3:
+            return three_numbers(number)
+
+        if len(str(number)) == 4:
+            return four_numbers(number)
+
+        if len(str(number)) == 5:
+            return five_numbers(number)
+
+        if len(str(number)) == 6:
+            return six_numbers(number)
+
+        if len(str(number)) in [7,8,9]:
+            return seveneightnine_numbers(number)
+
 
 def remove_0(number):
     """return a string whithout the left-sided zeros in a string-format number"""
@@ -14,31 +40,6 @@ def sum_base_10(number):
     for num in range(len(string)-1, -1, -1):
         numeros.append(int(string[len(string)-1-num])*10**num)
     return numeros
-        
-
-def five_numbers(number):
-    """return the name of a 5-digit number"""
-    if str(number)[2:] == '000':
-        return f'{englishnumber(str(number)[0:2])} thousand'
-    else:
-        return f'{englishnumber(str(number)[0:2])} thousand and {englishnumber(str(number)[2:])}'
-
-
-def four_numbers(number):
-    """return the name of a 4-digit number"""
-    if str(number)[1:] == '000':
-        return f'{MainNumbers[str(number)[0]]} thousand'
-    else:
-        return f'{MainNumbers[str(number)[0]]} thousand and {englishnumber(str(number)[1:])}'
-   
-
-def three_numbers(number):
-    """return the name of a 3-digit number"""
-    if str(number)[1:] == '00':
-        return f'{MainNumbers[str(number)[0]]} hundred'
-    else:
-        number = remove_0(str(number))
-        return f'{MainNumbers[str(number)[0]]} hundred and {two_numbers(remove_0(str(number)[1:]))}'
 
 
 def two_numbers(x):
@@ -51,25 +52,45 @@ def two_numbers(x):
         return f'{MainNumbers[str(aux[0])]}-{MainNumbers[str(aux[1])]}'
 
 
-def englishnumber(number):
-    """facilitates to call number names to make anothers number names"""
-    if number == 0:
-        return 'zero'
+def three_numbers(number):
+    """return the name of a 3-digit number"""
+    if str(number)[-2:] == '00':
+        return f'{MainNumbers[str(number)[0]]} hundred'
     else:
         number = remove_0(str(number))
-        if len(str(number)) <= 2:
-            return two_numbers(number)
-            
-        if len(str(number)) == 3:
-            return three_numbers(number)
+        return f'{MainNumbers[str(number)[0]]} hundred and {two_numbers(remove_0(str(number)[1:]))}'
 
-        if len(str(number)) == 4:
-            return four_numbers(number)
 
-        if len(str(number)) == 5:
-            return five_numbers(number)
+def four_numbers(number):
+    """return the name of a 4-digit number"""
+    if str(number)[-3:] == '000':
+        return f'{MainNumbers[str(number)[0]]} thousand'
+    else:
+        return f'{MainNumbers[str(number)[0]]} thousand and {englishnumber(str(number)[1:])}'
 
-                
+
+def five_numbers(number):
+    """return the name of a 5-digit number"""
+    if str(number)[-3:] == '000':
+        return f'{englishnumber(str(number)[0:2])} thousand'
+    else:
+        return f'{englishnumber(str(number)[0:2])} thousand and {englishnumber(str(number)[2:])}'
+
+      
+def six_numbers(number):
+    if str(number)[-3:] == '000':
+        return f'{englishnumber(str(number)[0:3])} thousand'
+    else:
+        return f'{englishnumber(str(number)[0:3])} thousand and {englishnumber(str(number)[3:])}'
+
+
+def seveneightnine_numbers(number):
+    if str(number)[-6:] == '000000':
+        return f'{englishnumber(str(number)[:-6])} million'
+    else:
+        return f'{englishnumber(str(number)[:-6])} million, {englishnumber(str(number)[-6:])}'
+
+
 MainNumbers = {'0':'zero',
                '1':'one',
                '2':'two',
