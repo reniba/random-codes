7a843f0ec8f2a62998d62b17de7e9f0b046105cf
reniba <112177930+reniba@users.noreply.github.com>
2022-12-28 12:35:41 -0300

    Update divisors.py

--- divisors.py
+++ divisors.py
@@ -1,5 +1,6 @@
-#fatorador
+
 def divisors(num):
+    """return a list with the positive divisors of the called-number"""
     divisores = []
     for a in range(1,num//2+1):
         if num % a == 0:
